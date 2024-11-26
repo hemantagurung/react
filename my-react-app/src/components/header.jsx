b3e10beeaef2728b031f8b5ae98ab0640cdb7fac
unknown <hemantatamu3@gmail.com>
2024-11-26 21:19:05 +0545

    this is react

---
+++ my-react-app/src/components/header.jsx
@@ -0,0 +1,16 @@
+
+
+import React from 'react'
+
+const header = () => {
+  return (
+    <div>
+    <header>
+    <h1>This is  a header</h1>
+    <p>this is a simple header components</p>
+    </header>
+    </div>
+  )
+}
+
+export default header
